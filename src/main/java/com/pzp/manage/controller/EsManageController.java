8b680ec8834bd9b625c1cff4ba1a469cd0a82f31
liguodong <liguodongiot@163.com>
2018-02-11 00:32:05 +0800

    整合jquery和bootstrap

--- src/main/java/com/pzp/manage/controller/EsManageController.java
+++ src/main/java/com/pzp/manage/controller/EsManageController.java
@@ -1,5 +1,13 @@
 package com.pzp.manage.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * <p>Project: pzp-operation-manage-system</p>
  * <p>Package: com.pzp.manage.controller</p>
@@ -10,7 +18,31 @@ package com.pzp.manage.controller;
  * @version 1.0.0
  * @date 2018/1/15 13:44 星期一
  */
+@Controller
+@RequestMapping("/esManage")
 public class EsManageController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EsManageController.class);
+
+    /**
+     * http://localhost:8888/esManage/index
+     * @return
+     */
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public ModelAndView index() {
+        return new ModelAndView("esManage");
+    }
+
+
+    /**
+     * http://localhost:8888/esManage/test
+     * @return
+     */
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    @ResponseBody
+    public String test() {
+        return "test";
+    }
 
 
 
